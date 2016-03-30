8d5e163d2bddcc981e7ea2007eaaa54ec63297b3
Danié <dsoutofernandez@danielcastelao.org>
2016-03-30 20:50:00 +0200

    Commit Module Data types

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -7,11 +7,11 @@
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        Un tutorial larguito para SXE...
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Daniel",
+    'website': "http://www.danielcastelao.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
